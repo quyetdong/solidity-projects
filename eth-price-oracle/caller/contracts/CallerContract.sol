08b909fc19ccdf5905552368c562b0ad4e4f60e1
Dong Quyet <quyetvdong@gmail.com>
2021-11-24 23:43:46 +0700

    eht price oracle

--- eth-price-oracle/caller/contracts/CallerContract.sol
+++ eth-price-oracle/caller/contracts/CallerContract.sol
@@ -1,15 +1,19 @@
 pragma solidity 0.8.10;
+
 //1. Import from the "./EthPriceOracleInterface.sol" file
 import "./EthPriceOracleInterface.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
-contract CallerContract {
+contract CallerContract is Ownable {
   // 2. Declare `EthPriceOracleInterface`
   EthPriceOracleInterface private oracleInstance;
   address private oracleAddress;
+  event newOracleAddressEvent(address oracleAddress);
 
-  function setOracleInstanceAddress (address _oracleInstanceAddress) public {
+  function setOracleInstanceAddress (address _oracleInstanceAddress) public onlyOwner {
     oracleAddress = _oracleInstanceAddress;
     //3. Instantiate `EthPriceOracleInterface`
     oracleInstance = EthPriceOracleInterface(oracleAddress);
+    emit newOracleAddressEvent(oracleAddress);
   }
 }
