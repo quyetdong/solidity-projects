8a786b530e71debfdd34d5b3d3f0ad7385085bce
Kent Dong <kent@vinova.com.sg>
2021-11-26 17:11:46 +0700

    oracle: update role to access control

--- eth-price-oracle/oracle/contracts/EthPriceOracle.sol
+++ eth-price-oracle/oracle/contracts/EthPriceOracle.sol
@@ -1,21 +1,39 @@
 pragma solidity 0.8.10;
 
-import "@openzeppelin/contracts/access/Roles.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./CallerContractInterface.sol";
 
-contract EthPriceOracle {
-    using Roles for Roles.Role;
-    Roles.Role private owners;
-    Roles.Role private oracles;
-
+contract EthPriceOracle is AccessControl {
     uint256 private randNonce = 0;
     uint256 private modulus = 1000;
+    uint numOracles = 0;
+    bytes32 public constant ORACLE_ROLE = keccak256("ORACLE_ROLE");
+
     mapping(uint256 => bool) pendingRequests;
     event GetLatestEthPriceEvent(address callerAddress, uint256 id);
     event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
+    event RemoveOracleEvent(address oracleAddress);
+    event AddOracleEvent(address oracleAddress);
 
-    constructor(address _owner) public {
-        owners.add(_owner);
+    constructor(address _owner) {
+        _setRoleAdmin(ORACLE_ROLE, DEFAULT_ADMIN_ROLE);
+        _setupRole(DEFAULT_ADMIN_ROLE, _owner);
+    }
+
+    function addOracle(address _oracle) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(!hasRole(ORACLE_ROLE, _oracle), "Already an oracle!");
+        grantRole(ORACLE_ROLE, _oracle);
+        numOracles++;
+        emit AddOracleEvent(_oracle);
+    }
+
+    function removeOracle(address _oracle) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(hasRole(ORACLE_ROLE, _oracle), "Not an oracle!");
+        // 3. Continue here
+        require(numOracles > 1, "Do not remove the last oracle!");
+        revokeRole(ORACLE_ROLE, _oracle);
+        numOracles--;
+        emit RemoveOracleEvent(_oracle);
     }
 
     function getLatestEthPrice() public returns (uint256) {
